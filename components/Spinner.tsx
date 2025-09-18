b6df3bd4febda060f3cfd92f44815884cb725618
Amir Hatami <hatamiamir055@email.com>
2025-09-18 15:30:31 +0330

    add spinner

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -3,7 +3,7 @@ import React from "react";
 interface SpinnerProps {
     size?: number;
     color?: string;
-};
+}
 
 const Spinner: React.FC<SpinnerProps> = ({size = 16, color = "gray"}) => {
     return (
