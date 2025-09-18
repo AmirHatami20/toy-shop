76e864f14047f140058b7c7d3e4bbbc3a8c0dd2a
Amir Hatami <hatamiamir055@email.com>
2025-09-18 15:35:51 +0330

    Fix Spinner

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -8,13 +8,10 @@ interface SpinnerProps {
 const Spinner: React.FC<SpinnerProps> = ({size = 16, color = "gray"}) => {
     return (
         <div
-            className={`animate-spin rounded-full border-2 border-t-transparent`}
-            style={{
-                width: size,
-                height: size,
-                borderColor: color,
-            }}
+            className={`animate-spin rounded-full border-2 border-t-transparent border-[${color}]`}
+            style={{width: size, height: size}}
         />
+
     );
 };
 
